1c25379f4eedc0ec8fa05fa25b0063ab2394fa84
Turya-Kalburgi <allindiaturya.kalburgi@gmail.com>
2025-07-29 22:01:13 +0530

    check leap year

---
+++ LeapYearCheck.java
@@ -0,0 +1,33 @@
+//should % 4
+//should not % 100
+//should % 100 && % 400
+
+import java.util.*;
+
+
+public class LeapYearCheck {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a year to check:");
+        int leapyear = sc.nextInt();
+
+        boolean x =(leapyear % 4 == 0);
+        boolean y = (leapyear % 100!= 0);
+        boolean z = (leapyear % 100 == 0 && leapyear %400 == 0);
+        
+
+        if(x &&( y || z)){
+            System.out.println(leapyear +"is a leap year!");
+        }
+        else{
+            System.out.println(leapyear +"is not a leap year !");
+        }
+        }   
+        }
+
+
+    
+    
+
