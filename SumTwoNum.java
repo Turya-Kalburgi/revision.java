67a6df8a0e442e330bdcbb0705353b7d04d8d922
Turya-Kalburgi <allindiaturya.kalburgi@gmail.com>
2025-07-31 01:11:25 +0530

    add SumTwoNum Java program

---
+++ SumTwoNum.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class SumTwoNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any digit:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter another digit:");
+        int b =sc.nextInt();
+
+        int sum = a+b;
+        System.out.println("the sum of give numbers is:" +sum);
+
+    }   
+    
+}
