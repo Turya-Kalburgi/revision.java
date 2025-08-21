b44a89c15df5e0b0a8d1d5836c030f5018dadd59
Turya-Kalburgi <allindiaturya.kalburgi@gmail.com>
2025-08-21 23:09:36 +0530

    HcmLcm

---
+++ basics/HcmLcm.java
@@ -0,0 +1,30 @@
+package basics;
+import java.util.*;
+
+public class HcmLcm {
+    public static void main(String main[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter second number:");
+        int b =sc.nextInt();
+
+        int x =a; int y = b;
+
+        while (b!= 0){
+            int temp =b;
+            b = a%b;
+            a = temp;
+        }
+
+        int hcf = a;
+        int lcm = (x * y) / hcf;
+
+        System.out.println("HCF is:"+ x+"and"+y+"="+hcf);
+        System.out.println("LCM is:"+ x+"and"+y+"="+lcm);
+        sc.close();
+    }
+}
